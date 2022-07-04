a5f84c011eed6876eae20edb9d8525effb4411a1
huiji072 <fmti0720@gmail.com>
2022-07-04 23:18:08 +0900

    init

--- hello.js
+++
@@ -1,9 +0,0 @@
-<Script type = "text/javascript">
-     {/* $대신 jQuery를 사용 */}
-    jQuery('body').html('hello world');
-</Script>
-
-<Script type = "text/javascript">
-    {/* $를 함수의 지역변수로 선언해서 외부에 있을지 모르는 타 라이브러리의 $와 충돌을 예빵 */}
-</Script>
-
